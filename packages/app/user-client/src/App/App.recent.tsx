8ff080a840495375e51e92af42b09aafe7bfd97f
Bong <qhdgkdbs@gmail.com>
2023-02-08 03:47:45 +0900

    style: 최근 배송 컴포넌트 초안 세팅

--- packages/app/user-client/src/App/App.recent.tsx
+++ packages/app/user-client/src/App/App.recent.tsx
@@ -1,8 +1,17 @@
 import { memo } from 'react'
+import { Spacer } from '../component/Layout'
+import { SubTitle } from '../component/Text'
 
 const Recent = () => {
   // 배송 중인 정보 카드 캐로셀
-  return <div>Enter</div>
+
+  return (
+    <div>
+      <SubTitle>최근 배송</SubTitle>
+      <Spacer height="1rem" />
+      <div>카드 캐로셀</div>
+    </div>
+  )
 }
 
 export default memo(Recent)
