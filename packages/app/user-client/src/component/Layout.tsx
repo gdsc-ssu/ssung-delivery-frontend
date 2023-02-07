f418e64947a21ba89692df354ba9ac3b954e1809
Bong <qhdgkdbs@gmail.com>
2023-02-08 03:22:38 +0900

    style: 초기 디자인 컴포넌트 레이아웃 세팅

---
+++ packages/app/user-client/src/component/Layout.tsx
@@ -0,0 +1,31 @@
+import styled from '@emotion/styled'
+import {
+  flexbox,
+  FlexboxProps,
+  color,
+  ColorProps,
+  typography,
+  TypographyProps,
+} from 'styled-system'
+
+const BodyContainer = styled.div`
+  margin: 0 auto;
+  max-width: 640px;
+  min-width: 320px;
+`
+
+const Spacer = styled.div(({ height }: { height?: string }) => [
+  `height: ${height}; width: 100%;`,
+])
+
+type FlexProps = ColorProps & TypographyProps & FlexboxProps & { gap?: string }
+const Flex = styled.div<FlexProps>`
+  ${flexbox};
+  ${color};
+  ${typography};
+  gap: ${({ gap }) => gap || '0'};
+  display: flex;
+  cursor: pointer;
+`
+
+export { BodyContainer, Spacer, Flex }
