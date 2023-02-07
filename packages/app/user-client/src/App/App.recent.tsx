a66b779406d7f33f4535f644fe0be695d935ac6c
Bong <qhdgkdbs@gmail.com>
2023-02-08 03:32:45 +0900

    chore: memo 최적화 적용

--- packages/app/user-client/src/App/App.recent.tsx
+++ packages/app/user-client/src/App/App.recent.tsx
@@ -1,6 +1,8 @@
+import { memo } from 'react'
+
 const Recent = () => {
   // 배송 중인 정보 카드 캐로셀
   return <div>Enter</div>
 }
 
-export default Recent
+export default memo(Recent)
--- packages/app/user-client/src/App/App.shipping.tsx
+++ packages/app/user-client/src/App/App.shipping.tsx
@@ -1,7 +1,9 @@
+import { memo } from 'react'
+
 const Shipping = () => {
   // 배송 중인 정보 카드 스택
   // 배송 중인 정보 카드 위치 (진행 상태)
   return <div>Enter</div>
 }
 
-export default Shipping
+export default memo(Shipping)
