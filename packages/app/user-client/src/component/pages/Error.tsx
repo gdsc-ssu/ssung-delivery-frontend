e7089fa88440a8a9b856c122d40fbf4d857ca04c
Bong <qhdgkdbs@gmail.com>
2023-02-08 03:50:18 +0900

    chore: 에러 관련 페이지 초안 구성

---
+++ packages/app/user-client/src/component/pages/Error.tsx
@@ -0,0 +1,5 @@
+const Error = () => {
+  return <div>에러 발생!</div>
+}
+
+export default Error
---
+++ packages/app/user-client/src/component/pages/NotFoundPage.tsx
@@ -0,0 +1,5 @@
+const NotFoundPage = () => {
+  return <div>아무것도 없어!</div>
+}
+
+export default NotFoundPage
