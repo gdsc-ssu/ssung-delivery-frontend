182bdb0a1e7f0e38ecdf922e469b4f2679937746
Bong <qhdgkdbs@gmail.com>
2023-02-08 03:45:52 +0900

    style: 배송 중 영역 초안 세팅

--- packages/app/user-client/src/App/App.shipping.tsx
+++ packages/app/user-client/src/App/App.shipping.tsx
@@ -1,9 +1,20 @@
 import { memo } from 'react'
+import { Flex, Spacer } from '../component/Layout'
+import { SubTitle } from '../component/Text'
 
 const Shipping = () => {
   // 배송 중인 정보 카드 스택
   // 배송 중인 정보 카드 위치 (진행 상태)
-  return <div>Enter</div>
+  return (
+    <div>
+      <SubTitle>배송 중</SubTitle>
+      <Spacer height="1rem" />
+      <Flex justifyContent={'space-between'}>
+        <div>카드 스택</div>
+        <div>배송 현재 진행 바</div>
+      </Flex>
+    </div>
+  )
 }
 
 export default memo(Shipping)
