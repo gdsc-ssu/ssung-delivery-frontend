7dc39211e6916b5462efb82a442003ab8d8b2a5a
yoo-jimin127 <dbwlals9936@naver.com>
2023-02-28 21:57:42 +0900

    chore : sender client - header

--- packages/app/sender-client/src/App/App.header.tsx
+++ packages/app/sender-client/src/App/App.header.tsx
@@ -1,5 +1,5 @@
 import { useState, memo } from 'react';
-import { ComponentContainer, Flex, Title} from '@common/ssung-ui/components';
+import { ComponentContainer, Flex, SubTitle} from '@common/ssung-ui/components';
 import styled from '@emotion/styled';
 
 interface SenderHeaderProps {
@@ -13,12 +13,36 @@ const Header = (props: SenderHeaderProps) => {
     <ComponentContainer>
         <Flex flexDirection={'row'} justifyContent={'space-between'}>
             <div>
-                <img src={props.logo} alt="logo" />
+                <img src={props.logo} alt="logo" style={{"marginLeft":"1.5rem"}} />
             </div>
-            <ProfileBox>
+            <form>
                 <Flex flexDirection={'row'} justifyContent={'space-between'}>
-                    <img src={props.profileThumb} alt='profile image' />
-                    <Title>{props.username} 님</Title>
+                    <input type="text" value="" 
+                        style={{
+                            "width":"450px",
+                            "height":"40px",
+                            "border":"none",
+                            "borderRadius":"0.3rem",
+                            "boxShadow":"3px 3px 3px lightgray",
+                        }}
+                    />
+                    <button type="button"
+                        style={{
+                            "width":"40px",
+                            "height":"40px",
+                            "backgroundColor":"#ffffff",
+                            "border":"none",
+                            "borderRadius":"0.3rem",
+                            "boxShadow":"3px 3px 3px lightgray",
+                            "margin":"0 0.5rem"
+                        }}
+                    >검색</button>
+                </Flex>
+            </form>
+            <ProfileBox>
+                <Flex flexDirection={'row'} justifyContent={'flex-start'} style={{"marginLeft":"0.5rem"}}>
+                    <img src={props.profileThumb} alt='profile image' style={{"width":"3rem"}} />
+                    <SubTitle style={{"marginTop":"1rem", "marginLeft":"1rem"}}>{props.username} 님</SubTitle>
                 </Flex>
             </ProfileBox>
         </Flex>
@@ -27,8 +51,11 @@ const Header = (props: SenderHeaderProps) => {
 }
 
 const ProfileBox = styled.div`
-    background-color: #ffff;
-    font-size: 12px;
+    width: 220px;
+    height: 50px;
+    background-color: #ffffff;
+    border-radius: 0.3rem;
+    box-shadow: 3px 3px 3px lightgray;
 `;
 
 export default Header;
--- packages/app/sender-client/src/App/index.tsx
+++ packages/app/sender-client/src/App/index.tsx
@@ -3,19 +3,25 @@ import { css } from '@emotion/react'
 import '../styles/global.css'
 import { BodyContainer, Spacer } from '../component/Layout'
 import { useCallback, useState } from 'react'
+import styled from '@emotion/styled';
 
 function App() {
     const [searchID, setSearchID] = useState<string>()
     const onSearchShipping = useCallback(() => {}, [])
 
     return (
-        <BodyContainer>
-            <Spacer height="3rem" />
+        <Container style={{"width":"100%", "backgroundColor":"#f3f3f3"}}>
+            <Spacer height="2rem" />
 
             <Header logo={'../logo.svg'} username={'카리나'} profileThumb={'../profile.svg'} />
             <Spacer height="3rem" />
-        </BodyContainer>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+`;
 
 export default App
