ced1137b58fa5de970b3c468d9bee737e0ede3a6
yoo-jimin127 <dbwlals9936@naver.com>
2023-02-28 20:57:48 +0900

    hotfix : monorepo dependencies error solve

--- packages/app/sender-client/src/App/index.tsx
+++ packages/app/sender-client/src/App/index.tsx
@@ -1,12 +1,19 @@
+import Header from './App.header'
 import { css } from '@emotion/react'
 import '../styles/global.css'
 import { BodyContainer, Spacer } from '../component/Layout'
 import { useCallback, useState } from 'react'
 
 function App() {
+    const [searchID, setSearchID] = useState<string>()
+    const onSearchShipping = useCallback(() => {}, [])
+
     return (
         <BodyContainer>
+            <Spacer height="3rem" />
 
+            <Header logo={'../logo.svg'} username={'카리나'} profileThumb={'../profile.svg'} />
+            <Spacer height="3rem" />
         </BodyContainer>
     )
 }
