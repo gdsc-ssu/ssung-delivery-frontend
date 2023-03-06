3f6f0649e949d0aa1fb70583df847b2b5452f2dd
yoo-jimin127 <dbwlals9936@naver.com>
2023-03-06 17:09:01 +0900

    chore : shiptable mockup data edit

--- packages/app/sender-client/src/Model/shiptable.mock.ts
+++ packages/app/sender-client/src/Model/shiptable.mock.ts
@@ -5,26 +5,27 @@ export const SHIP_TABLE_MOCK_DATA: ShipTableData[] = [
   {
     id: '1',
     keywords: ['기분이', '심심한', '숭실대'],
+    receiver: '유지민 010-9936-4524',
     product: '구글 픽셀 7',
     progress: '80%',
     history: [
       {
-        state: '배송중',
+        state: '배송중 🚛',
         date: '2020-01-01',
         done: true,
       },
       {
         state: '옥천 HUB',
         date: '2020-01-02',
-        done: true,
+        done: false,
       },
       {
-        state: '배송 출발',
+        state: '배송 출발 💨',
         date: '2020-01-02',
         done: false,
       },
       {
-        state: '배송 도착',
+        state: '배송 도착 📦',
         date: '2020-01-03',
         done: false,
       },
@@ -33,11 +34,12 @@ export const SHIP_TABLE_MOCK_DATA: ShipTableData[] = [
   {
     id: '2',
     keywords: ['슝슝한', '서울대', '대학생'],
+    receiver: '봉승우 010-1234-4321',
     product: '맥북프로 16',
     progress: '10%',
     history: [
       {
-        state: '배송중',
+        state: '배송중 🚛',
         date: '2020-01-01',
         done: true,
       },
@@ -47,12 +49,12 @@ export const SHIP_TABLE_MOCK_DATA: ShipTableData[] = [
         done: true,
       },
       {
-        state: '배송 출발',
+        state: '배송 출발 💨',
         date: '2020-01-02',
         done: false,
       },
       {
-        state: '배송 도착',
+        state: '배송 도착 📦',
         date: '2020-01-03',
         done: false,
       },
@@ -61,11 +63,12 @@ export const SHIP_TABLE_MOCK_DATA: ShipTableData[] = [
   {
     id: '3',
     keywords: ['배고픈', '서울대', '지킴이'],
+    receiver: '정명진 010-0000-1313',
     product: '아이폰 18',
     progress: '94%',
     history: [
       {
-        state: '배송중',
+        state: '배송중 🚛',
         date: '2020-01-01',
         done: true,
       },
@@ -75,12 +78,12 @@ export const SHIP_TABLE_MOCK_DATA: ShipTableData[] = [
         done: true,
       },
       {
-        state: '배송 출발',
+        state: '배송 출발 💨',
         date: '2020-01-02',
-        done: false,
+        done: true,
       },
       {
-        state: '배송 도착',
+        state: '배송 도착 📦',
         date: '2020-01-03',
         done: false,
       },
@@ -89,11 +92,12 @@ export const SHIP_TABLE_MOCK_DATA: ShipTableData[] = [
   {
     id: '4',
     keywords: ['배고픈', '관악산', '아이폰'],
+    receiver: '송지호 010-1212-3434',
     product: '애플워치',
     progress: '100%',
     history: [
       {
-        state: '배송중',
+        state: '배송중 🚛',
         date: '2020-01-01',
         done: true,
       },
@@ -103,12 +107,12 @@ export const SHIP_TABLE_MOCK_DATA: ShipTableData[] = [
         done: true,
       },
       {
-        state: '배송 출발',
+        state: '배송 출발 💨',
         date: '2020-01-02',
         done: true,
       },
       {
-        state: '배송 도착',
+        state: '배송 도착 📦',
         date: '2020-01-03',
         done: true,
       },
@@ -117,28 +121,29 @@ export const SHIP_TABLE_MOCK_DATA: ShipTableData[] = [
   {
     id: '5',
     keywords: ['도착한', '관악산', '스티브'],
+    receiver: '김구글 010-0121-5656',
     product: '아이패드 프로 12',
     progress: '100%',
     history: [
       {
-        state: '배송중',
+        state: '배송중 🚛',
         date: '2020-01-01',
         done: true,
       },
       {
         state: '옥천 HUB',
         date: '2020-01-02',
-        done: true,
+        done: false,
       },
       {
-        state: '배송 출발',
+        state: '배송 출발 💨',
         date: '2020-01-02',
-        done: true,
+        done: false,
       },
       {
-        state: '배송 도착',
+        state: '배송 도착 📦',
         date: '2020-01-03',
-        done: true,
+        done: false,
       },
     ],
   },
