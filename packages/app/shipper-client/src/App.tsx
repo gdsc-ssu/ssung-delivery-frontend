c6c144a0d28729239e049f156e00840fed370458
Bong <qhdgkdbs@gmail.com>
2023-03-09 14:08:23 +0900

    style: move default css to ssung-ui

--- packages/app/shipper-client/src/App.tsx
+++ packages/app/shipper-client/src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 function App() {
-  return <div className="App">shipper-client</div>;
+  return <div className="App">shipper-client</div>
 }
 
-export default App;
+export default App
