87ce3e91599637bc711fc5053db5df83abdcf8b8
Bong <qhdgkdbs@gmail.com>
2023-03-10 16:32:00 +0900

    style: set ing, ed info with style

--- packages/app/shipper-client/src/App/App.shippingList.tsx
+++ packages/app/shipper-client/src/App/App.shippingList.tsx
@@ -1,13 +1,26 @@
-import { ComponentContainer, Spacer, Title } from '@common/ssung-ui'
-import ShippingBox from './Shipping/GroundForm'
+import { ComponentContainer, Flex, Spacer, Title } from '@common/ssung-ui'
+import styled from '@emotion/styled'
 import InfoCard from './Shipping/InfoCard'
 import GroundForm from './Shipping/GroundForm'
 import Stack from './Shipping/Stack'
+import { Text } from '@common/ssung-ui'
 
 const ShippingList = () => {
   return (
     <ComponentContainer>
-      <Title>배송목록</Title>
+      <Flex justifyContent={'space-between'}>
+        <Title>배송목록</Title>
+        <Row>
+          <Row>
+            <Text>배송 중</Text>
+            <ShippingIng type="primary">1232</ShippingIng>
+          </Row>
+          <Row>
+            <Text>배송완료</Text>
+            <ShippingIng type="negative">24</ShippingIng>
+          </Row>
+        </Row>
+      </Flex>
       <Spacer height="2rem" />
 
       <Stack Top={InfoCard} Ground={GroundForm} />
@@ -17,3 +30,14 @@ const ShippingList = () => {
 }
 
 export default ShippingList
+
+const ShippingIng = styled(Text)(
+  ({ type }: { type: 'primary' | 'negative' }) => [
+    `color: white; background-color: var(--${type}); padding: 0.25rem; border-radius: 1rem;`,
+  ]
+)
+
+const Row = styled(Flex)`
+  align-items: center;
+  gap: 0.5rem;
+`
