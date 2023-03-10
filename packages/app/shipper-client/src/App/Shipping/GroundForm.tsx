7a9154af28a8b43ef0bc32ed9e314c636695acd1
Bong <qhdgkdbs@gmail.com>
2023-03-10 16:10:13 +0900

    feat: add shipping ground Form Component

---
+++ packages/app/shipper-client/src/App/Shipping/GroundForm.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled'
+
+const ShippingBox = () => {
+  return <Container></Container>
+}
+
+export default ShippingBox
+
+const Container = styled.div`
+  background-color: var(--negative);
+  height: 100%;
+  width: 100%;
+  border-radius: 1rem;
+`
