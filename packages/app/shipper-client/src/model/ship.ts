313bba552e9002dc7000ad8c6cde9c8769319c42
Bong <qhdgkdbs@gmail.com>
2023-03-14 20:01:03 +0900

    chore(sender): add type, mock for shipping data

---
+++ packages/app/shipper-client/src/model/ship.ts
@@ -0,0 +1,23 @@
+// 배송 관련 데이터  interface, type 정의
+// 서버에서 내려오는 타입들 정의
+
+export interface SenderType {
+  name: string
+  phone: string
+}
+
+export interface ShipHistoryType {
+  state: string
+  date: string
+  done: boolean
+}
+
+export interface ShipData {
+  id: string
+  keywords: string[]
+  product: string
+  progress: string
+  destination: string
+  history: ShipHistoryType[]
+  sender: SenderType
+}
