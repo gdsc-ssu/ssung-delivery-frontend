b6c238283bb9245ed5cd5b44423a2a08e3f68395
Bong <qhdgkdbs@gmail.com>
2023-03-14 19:39:27 +0900

    style(sender): add shipping ground box form

--- packages/app/shipper-client/src/App/Shipping/GroundForm.tsx
+++ packages/app/shipper-client/src/App/Shipping/GroundForm.tsx
@@ -1,10 +1,62 @@
 import styled from '@emotion/styled'
+import { Flex, Text } from '@common/ssung-ui'
 
 const ShippingBox = () => {
-  return <Container></Container>
+  return (
+    <Container>
+      <SafeArea>
+        <FormConainer>
+          <Row>
+            <Text>배송상태</Text>
+            <FormSelect>
+              <option>수거 완료</option>
+              <option>배송 중</option>
+              <option>배송 완료</option>
+            </FormSelect>
+          </Row>
+
+          <Row>
+            <Text>물품위치</Text>
+            <FormSelect>
+              <option>서울</option>
+              <option>경기도</option>
+              <option>제주도</option>
+            </FormSelect>
+          </Row>
+
+          <Row>
+            <button>UPDATE</button>
+          </Row>
+        </FormConainer>
+      </SafeArea>
+    </Container>
+  )
 }
 
 export default ShippingBox
+
+const FormSelect = styled.select`
+  min-width: 8rem;
+`
+
+const FormConainer = styled(Flex)`
+  flex-direction: column;
+  height: 100%;
+  justify-content: space-evenly;
+`
+
+const Row = styled(Flex)`
+  align-items: center;
+  justify-content: space-evenly;
+  width: 100%;
+`
+
+const SafeArea = styled.div`
+  height: 100%;
+  margin-left: 20%;
+  margin-right: 10%;
+  border-radius: 1rem;
+`
 
 const Container = styled.div`
   background-color: var(--negative);
