9ce85540126e70ae349f61f1e570d5be5e8b68b4
Bong <qhdgkdbs@gmail.com>
2023-03-16 16:35:46 +0900

    feat(sender): publish dash updater component with transition

---
+++ packages/app/shipper-client/src/components/DashUpdater.tsx
@@ -0,0 +1,39 @@
+import styled from '@emotion/styled'
+import { CameraOnly, Flex, Text, Title } from '@common/ssung-ui'
+
+const DashUpdater = () => {
+  return (
+    <Container>
+      <CamContainer>
+        <CameraOnly />
+      </CamContainer>
+
+      <FormContainer>
+        <Title>기분이 슝슝한 서울대</Title>
+        <Text>배송상태</Text>
+        <Text>위치</Text>
+        <button>UPDATE</button>
+      </FormContainer>
+    </Container>
+  )
+}
+
+export default DashUpdater
+
+const FormContainer = styled(Flex)`
+  flex-direction: column;
+  padding: 1rem;
+  gap: 0.5rem;
+`
+
+const CamContainer = styled.div`
+  width: 320px;
+  height: 320px;
+  position: relative;
+`
+
+const Container = styled.div`
+  background-color: white;
+  padding: 0.25rem;
+  border-radius: 0.5rem;
+`
