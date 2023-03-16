41461672cdca3d0d37f02557b08910dcd7f5398b
Bong <qhdgkdbs@gmail.com>
2023-03-16 17:42:27 +0900

    feat(sender): update parsed qr data to update dash

--- packages/app/shipper-client/src/components/DashUpdater.tsx
+++ packages/app/shipper-client/src/components/DashUpdater.tsx
@@ -1,18 +1,43 @@
 import styled from '@emotion/styled'
-import { CameraOnly, Flex, Text, Title } from '@common/ssung-ui'
+import { CameraOnly, Flex, Spacer, Text, Title } from '@common/ssung-ui'
+import { useEffect, useState } from 'react'
+import { WAREHOUSE_LOCATION, SHIPPING_STEP } from '../model/location'
 
 const DashUpdater = () => {
+  const [parsedQR, setParsedQR] = useState<string>('')
+
+  const onUpdateShippingState = () => {
+    if (!parsedQR) return alert('인식된 정보가 없습니다.')
+    alert(parsedQR)
+  }
+
   return (
     <Container>
       <CamContainer>
-        <CameraOnly />
+        <CameraOnly setResult={setParsedQR} />
       </CamContainer>
 
       <FormContainer>
-        <Title>기분이 슝슝한 서울대</Title>
-        <Text>배송상태</Text>
-        <Text>위치</Text>
-        <button>UPDATE</button>
+        <Title>{parsedQR || 'QR을 인식해보세요!'}</Title>
+        <Spacer height="0.5rem" />
+        <FormRow>
+          <LabelForForm>배송상태</LabelForForm>{' '}
+          <Select>
+            {SHIPPING_STEP.map((step) => (
+              <option>{step}</option>
+            ))}
+          </Select>
+        </FormRow>
+        <Flex>
+          <LabelForForm>위치</LabelForForm>
+          <Select>
+            {WAREHOUSE_LOCATION.map((loc) => (
+              <option>{loc.name}</option>
+            ))}
+          </Select>
+        </Flex>
+        <Spacer height="0.5rem" />
+        <button onClick={onUpdateShippingState}>UPDATE</button>
       </FormContainer>
     </Container>
   )
@@ -20,10 +45,25 @@ const DashUpdater = () => {
 
 export default DashUpdater
 
+const FormRow = styled(Flex)`
+  height: 1.5rem;
+  align-items: center;
+`
+
+const LabelForForm = styled(Text)`
+  width: 5rem;
+  flex-shrink: 0;
+`
+
+const Select = styled.select`
+  width: 100%;
+`
+
 const FormContainer = styled(Flex)`
   flex-direction: column;
   padding: 1rem;
   gap: 0.5rem;
+  z-index: 5;
 `
 
 const CamContainer = styled.div`
