a7bb643bdf683d785f8e4a5231940155f0f2f39a
yoo-jimin127 <dbwlals9936@naver.com>
2023-03-16 14:33:06 +0900

    chore(sender) : Modal component import

--- packages/app/sender-client/src/App/App.upload.tsx
+++ packages/app/sender-client/src/App/App.upload.tsx
@@ -1,19 +1,18 @@
+import { read as xRead, utils as xUtils } from "xlsx";
 import { useState, memo } from 'react';
-import { BorderButton, Button, Card, ComponentContainer, Flex, SubTitle, PrintLabelCard, Modal} from '@common/ssung-ui/components';
+import { Button, Card, ComponentContainer, Flex, SubTitle, PrintLabelCard } from '@common/ssung-ui/components';
 import styled from '@emotion/styled';
 import { PRINT_LABEL_MOCK_DATA } from "../Model/printlabel.mock"
 import { PrintLabelData } from "../Model/printlabel"
+import Modal from "./Modal/Modal";
 
 const Upload = () => {
-    const [showModal, setShowModal] = useState(false);
+    /** 모달창 노출 여부 state */
+    const [modalOpen, setModalOpen] = useState<boolean>(false);
 
-    const openModal = () => {
-        setShowModal(true);
-        console.log("open modal button")
-    }
-
-    const closeModal = () => {
-        setShowModal(false);
+    /** 모달창 노출 함수 */
+    const showModal = () => {
+        setModalOpen(true);
     };
 
     return (
@@ -25,12 +24,38 @@ const Upload = () => {
                 <Flex style={{"marginRight":"2.5rem", "marginTop":"2.5rem"}}>
                     <BorderButton>운송정보 업로드</BorderButton>
                     <BorderButton>운송정보 엑셀 업로드</BorderButton>
-                    <BorderButton onClick={openModal}>라벨지 출력</BorderButton>
-                    <Modal showModal={showModal} closeModal={closeModal} />
+                    <div>
+                        <BorderButton onClick={showModal}>라벨지 출력</BorderButton>
+                            {modalOpen && <Modal setModalOpen={setModalOpen}
+                                content={
+                                    <div>
+                                        모달 내용
+                                    </div>
+                                }
+                            />}
+                    </div>
                 </Flex>
             </Flex>
         </ComponentContainer>
     )
 }
+
+const BorderButton = styled.button`
+    height: 2.5rem;
+    padding: 0 10px;
+    font-size: 0.9rem;
+    font-weight: 600;
+    line-height: 1.5;
+    margin: 1rem 0.25rem;
+    background-color: #ffffff;
+    color: #07D39F;
+    border-radius: 5px;
+    border: 1px solid #07D39F;
+    box-shadow: 1px 1px 5px lightgray;
+
+    &:hover {
+        box-shadow: 3px 3px 5px lightgray;
+    }
+`;
 
 export default memo(Upload)
