6e8190d4655fcf5fc4536447f5558831b898c985
Bong <qhdgkdbs@gmail.com>
2023-03-16 19:59:00 +0900

    chore(sender): move shipping card stack component

---
+++ packages/app/shipper-client/src/components/Shipping/GroundForm.tsx
@@ -0,0 +1,79 @@
+import styled from '@emotion/styled'
+import { Flex, Text } from '@common/ssung-ui'
+import { WAREHOUSE_LOCATION, SHIPPING_STEP } from '../../model/location'
+
+interface ShippingBoxType {
+  id: string
+  state: string
+  updateState: (id: string, toState: string) => void
+}
+
+const ShippingBox = (props: ShippingBoxType) => {
+  const updateState = () => {
+    props.updateState(props.id, '')
+  }
+
+  return (
+    <Container isFinish={props.state === '배송완료'}>
+      <SafeArea>
+        <FormConainer>
+          <Row>
+            <Text>배송상태</Text>
+            <FormSelect>
+              {SHIPPING_STEP.map((step) => (
+                <option>{step}</option>
+              ))}
+            </FormSelect>
+          </Row>
+
+          <Row>
+            <Text>물품위치</Text>
+            <FormSelect>
+              {WAREHOUSE_LOCATION.map((loc) => (
+                <option>{loc.name}</option>
+              ))}
+            </FormSelect>
+          </Row>
+
+          <Row>
+            <button onClick={updateState}>UPDATE</button>
+          </Row>
+        </FormConainer>
+      </SafeArea>
+    </Container>
+  )
+}
+
+export default ShippingBox
+
+const FormSelect = styled.select`
+  min-width: 8rem;
+`
+
+const FormConainer = styled(Flex)`
+  flex-direction: column;
+  height: 100%;
+  justify-content: space-evenly;
+`
+
+const Row = styled(Flex)`
+  align-items: center;
+  justify-content: space-evenly;
+  width: 100%;
+`
+
+const SafeArea = styled.div`
+  height: 100%;
+  margin-left: 20%;
+  margin-right: 10%;
+  border-radius: 1rem;
+`
+
+const Container = styled.div(
+  ({ isFinish }: { isFinish: boolean }) => `
+  background-color: ${isFinish ? 'var(--primary)' : 'var(--negative)'};
+  height: 100%;
+  width: 100%;
+  border-radius: 1rem;
+`
+)
---
+++ packages/app/shipper-client/src/components/Shipping/Stack.tsx
@@ -0,0 +1,64 @@
+import styled from '@emotion/styled'
+import { ReactNode, useState } from 'react'
+import { BiDotsVerticalRounded } from 'react-icons/bi'
+
+interface StackProps {
+  Top: ReactNode
+  Ground: ReactNode
+}
+
+const Stack = ({ Top, Ground }: StackProps) => {
+  const [open, setOpen] = useState<boolean>(false)
+
+  return (
+    <Container>
+      <StackTop open={open}>{Top}</StackTop>
+      <StackGround>{Ground}</StackGround>
+      <Arrow
+        open={open}
+        onClick={() => {
+          setOpen(!open)
+        }}
+      >
+        <BiDotsVerticalRounded size={'1.75rem'} color={'white'} />
+      </Arrow>
+    </Container>
+  )
+}
+
+export default Stack
+
+const Arrow = styled.div(
+  ({ open }: { open: boolean }) => `
+  position: absolute;
+  display: flex;
+  align-items: center;
+  top: 0;
+  bottom : 0;
+  margin: auto;
+  right: 0;
+  transform: rotate(${open ? '-180deg' : '0'});
+  transition: all ease 0.5s;
+  `
+)
+
+const Container = styled.div`
+  position: relative;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 15px 0px;
+  border-radius: 1rem;
+`
+
+const StackTop = styled.div(
+  ({ open }: { open: boolean }) => `
+    position: absolute;
+    width: 100%;
+    height: 140px;
+    transition: all ease 0.5s;
+    transform: translate3d(${open ? '-80%' : '0'}, 0px, 0);
+  `
+)
+const StackGround = styled.div`
+  z-index: -2;
+  width: 100%;
+  height: 140px;
+`
