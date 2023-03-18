248d42f9d85381a14dbd57fc53cb4732e1489937
yoo-jimin127 <dbwlals9936@naver.com>
2023-03-18 11:31:44 +0900

    chore(ssung-ui) : update CustomForm component(add form data state & button click event logic)

--- packages/common/ssung-ui/components/CustomForm.tsx
+++ packages/common/ssung-ui/components/CustomForm.tsx
@@ -1,15 +1,14 @@
 import styled from '@emotion/styled'
 import { ComponentContainer, Flex, Spacer } from './Layout'
-import Button from './Button'
 import { Text } from './Text'
 import ProgressBar from './ProgressBar'
-import { memo, useState } from 'react'
-import { DatePicker, Space } from 'antd'
+import { memo, useState} from 'react'
+import { DatePicker } from 'antd'
 
 interface ShipState {
     value: string,
     disabled?: boolean,
-    name: string
+    name: string,
 }
 
 /**
@@ -17,16 +16,67 @@ interface ShipState {
  */
 const OPTIONS: ShipState[] = [
     {value: "default", name:"배송 상태를 선택해주세요."},
-    {value: "sending", name:"발송"},
-    {value: "collecting", name:"집하"},
-    {value: "shipping", name:"배송 중"},
-    {value: "arrival", name:"배송 완료"},
+    {value: "ordered", name:"발송"},
+    {value: "shipping", name:"집하"},
+    {value: "outForDelivery", name:"배송 중"},
+    {value: "shipped", name:"배송 완료"},
 ];
 
 /** antd date range picker */
 const { RangePicker } = DatePicker;
 
 const CustomForm = () => {
+    /** customForm data state */
+    const [customFormData, setCustomFormData] = useState({
+        name: "",
+        tel: "",
+        label: "",
+        date: "",
+        shipstate: "",
+    });
+
+    /** form data state save function */
+    const handleChange = (event: any) => {
+        console.log(event.target.value)
+        setCustomFormData((prevFormData) => {
+            return {
+                ...prevFormData,
+                [event.target.name]: event.target.value,
+            };
+        });
+    }
+
+    /** form data state reset button function */
+    const resetFormData = () => {
+        setCustomFormData({
+            name: "",
+            tel: "",
+            label: "",
+            date: "",
+            shipstate: "",
+        });
+    };
+
+    /** select box component */
+    const SelectBox = (props: ShipState) => {
+        return (
+            <Select
+                id="shipstate" name="shipstate"
+                value={customFormData.shipstate}
+                onChange={handleChange}
+            >
+                {props.options.map((option) => (
+                    <option
+                        key={option.key}
+                        value={option.value}
+                    >
+                        {option.name}
+                    </option>
+                ))}
+            </Select>
+        );
+    };
+
     return (
         <FormContainer>
             <GridForm>
@@ -34,67 +84,63 @@ const CustomForm = () => {
                     <Label id="name">이름</Label>
                     <InputForm 
                         type="text"
-                        id="name"
-                        name="name"
-                        value=""
+                        id="name" name="name"
+                        value={customFormData.name}
+                        onChange={handleChange}
                         placeholder="이름" />
                 </NameInput>
                 <TelInput>
                     <Label id="tel">전화번호</Label>
                     <InputForm
                         type="tel"
-                        id="tel"
-                        name="tel"
-                        value=""
+                        id="tel" name="tel"
+                        value={customFormData.tel}
+                        onChange={handleChange}
                         placeholder="전화번호" />
                 </TelInput>
                 <LabelInput>
                     <Label id="label">라벨지</Label>
                     <InputForm
                         type="text"
-                        id="label"
-                        name="label"
-                        value=""
+                        id="label" name="label"
+                        value={customFormData.label}
+                        onChange={handleChange}
                         placeholder="식별자를 입력해주세요" />
                 </LabelInput>
                 <DateInput>
                     <Label id="date">날짜</Label>
-                    <RangePicker style={{
-                        "marginLeft":"0.5rem",
-                        "width":"32rem",
-                        "height":"2rem",
-                        "border":"none",
-                        "boxShadow":"3px 3px 10px lightgray",
-                    }} 
+                    <RangePicker 
+                        id="date" name="date"
+                        // value={customFormData.date}
+                        // onChange={handleChange}
+                        style={{
+                            "marginLeft":"0.5rem",
+                            "width":"32rem",
+                            "height":"2rem",
+                            "border":"none",
+                            "boxShadow":"3px 3px 10px lightgray",
+                        }} 
                     />
                 </DateInput>
                 <StatusInput>
                     <Label id="shipstate">배송 상태</Label>
-                    <SelectBox options={OPTIONS} defaultValue="default"/>
+                    <SelectBox options={OPTIONS} 
+                        value={customFormData.shipstate}
+                        onChange={handleChange}
+                    />
                 </StatusInput>
                 <BtnBox>
-                    <Button color="#7b7b7b" bgcolor="#ffffff">초기화</Button>
-                    <Button color="#ffffff" bgcolor='#07d39f'>검색</Button>
+                    <Button style={{"color":"#7b7b7b", "backgroundColor":"#ffffff"}}
+                        onClick={resetFormData}
+                    >초기화</Button>
+                    <Button style={{"color":"#ffffff", "backgroundColor":"#07d39f"}}
+                        onClick={() => console.log(customFormData)}
+                    >검색</Button>
                 </BtnBox>
             </GridForm>
         </FormContainer>
     );
 }
-
-const SelectBox = (props: ShipState) => {
-    return (
-        <Select>
-            {props.options.map((option) => (
-                <option
-                    value={option.value}
-                    defaultValue={props.defaultValue === option.value}
-                >
-                    {option.name}
-                </option>
-            ))}
-        </Select>
-    );
-};
 
 
 const FormContainer = styled.div`
@@ -192,6 +238,18 @@ const BtnBox = styled.div`
     display: flex;
     flex-direction: column;
     margin: 0.8rem 1rem 0 1rem;
+`;
+
+const Button = styled.button`
+    width: 80px;
+    height: 30px;
+    padding: 5px 10px;
+    font-size: 1rem;
+    line-height: 1.5;
+    margin: 15px;
+    border-radius: 5px;
+    border: none;
+    box-shadow: 3px 10px 10px lightgray;
 `;
 
 export default memo(CustomForm)
