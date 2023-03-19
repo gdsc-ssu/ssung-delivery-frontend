3ea91353838baac68db36376ca90719ca5f51598
yoo-jimin127 <dbwlals9936@naver.com>
2023-03-19 21:31:02 +0900

    chore(sender) : add Dashboard layout

--- packages/app/sender-client/src/App/index.tsx
+++ packages/app/sender-client/src/App/index.tsx
@@ -1,12 +1,14 @@
 import Header from './App.header'
 import { css } from '@emotion/react'
-import { BodyContainer, Spacer } from '../component/Layout'
+import { BodyContainer, Spacer, Flex } from '../component/Layout'
+import { Text } from '../component/Text'
 import { useCallback, useState } from 'react'
 import styled from '@emotion/styled'
 import Form from './App.form'
 import ShipTable from './App.shipTable'
-import { NotFound, Error, ComponentContainer } from '@common/ssung-ui/components'
+import { NotFound, Error, ComponentContainer, Title } from '@common/ssung-ui/components'
 import ShipInfo from './App.shipInfo'
+import Button from '@common/ssung-ui/components/Form/Button'
 
 function App() {
     /** 로그인 여부 상태 */
@@ -27,7 +29,29 @@ function App() {
                         <ShipTable />
                     </SenderWrapper>
                 </ComponentContainer>
-            : <div>대시보드</div>}
+            : 
+                <ComponentContainer>
+                    <Spacer height="2rem" />
+                    <Header logo={'../logo.svg'} username={'카리나'} profileThumb={'../profile.svg'} />
+                    <Spacer height="2rem" />
+                    <SenderWrapper>
+                        <Spacer height="2rem" />
+                        <Flex flexDirection={'column'} justifyContent={'center'}>
+                            <Spacer height="2rem" />
+                            <Title style={{"margin":"0 auto", "textAlign":"center", "fontSize":"1.8rem"}}>
+                                A new <span style={{"color":"var(--primary)"}}>waybill system</span> for Me and the Environment
+                            </Title>
+                            <Spacer height="5rem" />
+                            <DashboardImg src={'../dashboard.svg'} />
+                            <Spacer height="5rem" />
+                            <Button style={{"width":"15rem", "alignSelf":"center"}}>
+                                <Text color={"white"}>로그인</Text>
+                            </Button>
+                            <Spacer height="10rem" />
+                        </Flex>
+                    </SenderWrapper>
+                </ComponentContainer>
+            }
         </Container>
     )
 }
@@ -47,5 +71,10 @@ const SenderWrapper = styled.div`
   box-shadow: -2px -2px 15px lightgray;
   border-radius: 1.5rem 0 0 0;
 `
+
+const DashboardImg = styled.img`
+    width: 40rem;
+    margin: 0 auto;
+`;
 
 export default App
