59eff981c5768c4e1cc5ec50a5c67b5ea3e39979
Bong <qhdgkdbs@gmail.com>
2023-03-21 17:42:20 +0900

    Squashed commit of the following:
    commit 38719e5d608f4890f096485cf7235b8af9bdfc3f
    Merge: 739eb5b f43340e
    Author: Bong <qhdgkdbs@gmail.com>
    Date:   Tue Mar 21 17:37:04 2023 +0900
    
        Merge branch 'main' of https://github.com/gdsc-ssu/ssung-delivery-frontend
    
    commit f43340ec7aaa9557f33550a531bddbcd954ad8d5
    Merge: eb3016a 43602ba
    Author: Jimin Yoo <66112716+yoo-jimin127@users.noreply.github.com>
    Date:   Tue Mar 21 13:40:46 2023 +0900
    
        Merge pull request #44 from gdsc-ssu/feat/sender#43
    
        Feat/user#43 Connect User API
    
    commit eb3016ac8fb747b1dc0a03fe2f761124f2d31bb7
    Merge: d3658d3 1b0c2c8
    Author: Jimin Yoo <66112716+yoo-jimin127@users.noreply.github.com>
    Date:   Tue Mar 21 13:39:56 2023 +0900
    
        Merge pull request #41 from gdsc-ssu/feat/sender#40
    
        Feat/sender#40 Dashboard & Auth API
    
    commit 43602baf0547a790f2a7e323a38e0e8ec88d2a54
    Author: yoo-jimin127 <dbwlals9936@naver.com>
    Date:   Tue Mar 21 13:38:46 2023 +0900
    
        feat(user) : add user(crew) api connect source
    
    commit bf67d2fbe2bd9a64a836d61eda465a1e1b47dd6d
    Author: yoo-jimin127 <dbwlals9936@naver.com>
    Date:   Tue Mar 21 11:32:19 2023 +0900
    
        feat(user) : attach signup & signin modal in user-client
    
    commit 1b0c2c8634f722a264c7192acb695f94eaa4f65a
    Author: yoo-jimin127 <dbwlals9936@naver.com>
    Date:   Tue Mar 21 11:10:54 2023 +0900
    
        style(sender) : restyling sender header
    
    commit d77b6e788cfe67ab8a9d42ddb7c5d6a250e33a65
    Author: yoo-jimin127 <dbwlals9936@naver.com>
    Date:   Tue Mar 21 10:50:58 2023 +0900
    
        style(sender) : upload dashboard assets
    
    commit b9d2f764b3d7caa7ebe7a28c78066810d1ea1a9e
    Author: yoo-jimin127 <dbwlals9936@naver.com>
    Date:   Mon Mar 20 14:43:46 2023 +0900
    
        chore(ssung-ui) : edit auth - sender/create parameters
    
    commit 653cd8c8e18d91f838f5e344574461ec87d58a21
    Author: yoo-jimin127 <dbwlals9936@naver.com>
    Date:   Mon Mar 20 12:17:25 2023 +0900
    
        style(ssung-ui) : edit request result alert position
    
    commit 2e1162cdc6f276f3b9df0c94a04f50b05e7c8aca
    Author: yoo-jimin127 <dbwlals9936@naver.com>
    Date:   Mon Mar 20 01:21:08 2023 +0900
    
        feat(ssung-ui) : add signin & signup api integrate code
    
    commit 155312de06221453042ba966bb6c2f02d1a915f8
    Author: yoo-jimin127 <dbwlals9936@naver.com>
    Date:   Mon Mar 20 01:20:45 2023 +0900
    
        feat(ssung-ui) : add signin & signup api integrate code
    
    commit 6fe061ce15f9ad8d2d54d7d833ac513c0f40839d
    Author: yoo-jimin127 <dbwlals9936@naver.com>
    Date:   Sun Mar 19 23:56:04 2023 +0900
    
        feat(ssung-ui) : add signup state
    
    commit 1b0e9a2b6444a16fead594be6cc820ce0c738236
    Author: yoo-jimin127 <dbwlals9936@naver.com>
    Date:   Sun Mar 19 23:55:39 2023 +0900
    
        feat(ssung-ui) : add login state
    
    commit fbe96abff13fc392bb57dd1dbf843fb66f8c2728
    Author: yoo-jimin127 <dbwlals9936@naver.com>
    Date:   Sun Mar 19 23:12:58 2023 +0900
    
        build(sender) : install react-router-dom
    
    commit 7f2e7d7b32378fd88afd2a8e132c15bd6b47b66a
    Author: yoo-jimin127 <dbwlals9936@naver.com>
    Date:   Sun Mar 19 23:12:05 2023 +0900
    
        test(sender & auth) : HOC component
    
    commit c1f301b6f81c5dbc506097df9569d58926a588d9
    Author: yoo-jimin127 <dbwlals9936@naver.com>
    Date:   Sun Mar 19 23:07:19 2023 +0900
    
        style(sender) : separate dashboard component
    
    commit 3ea91353838baac68db36376ca90719ca5f51598
    Author: yoo-jimin127 <dbwlals9936@naver.com>
    Date:   Sun Mar 19 21:31:02 2023 +0900
    
        chore(sender) : add Dashboard layout
    
    commit 57e037888a0e88f8d7f4dbb7affe3d55053a04f1
    Author: yoo-jimin127 <dbwlals9936@naver.com>
    Date:   Sun Mar 19 19:29:13 2023 +0900
    
        feat(sender) : add isSignIn status & conditional rendering

--- packages/app/user-client/src/App/App.header.tsx
+++ packages/app/user-client/src/App/App.header.tsx
@@ -1,10 +1,22 @@
-import { ComponentContainer, Flex, Title } from '@common/ssung-ui/components'
+import { ComponentContainer, Flex, Title, SignUp, SignIn } from '@common/ssung-ui/components'
 import { memo } from 'react'
+import { useModal } from '@common/ssung-ui'
 // import CONFIG from 'site.config'
 
 const Header = () => {
+  const { Modal: SignInModal, onModalOpen: onSignInOpen } = useModal()
+  const { Modal: SignUpModal, onModalOpen: onSignUpOpen } = useModal()
+
   return (
     <ComponentContainer>
+      <SignInModal>
+        <SignIn moveToSignUp={onSignUpOpen} />
+      </SignInModal>
+
+      <SignUpModal>
+        <SignUp />
+      </SignUpModal>
+
       <Flex flexDirection={'row'} justifyContent={'space-between'}>
         <div>
           <Title>
@@ -14,7 +26,7 @@ const Header = () => {
           <Title>배송 중이에요.</Title>
         </div>
         <div>
-          <img src={'./profile.svg'} alt="프로필 사진" />
+          <img src={'./profile.svg'} alt="프로필 사진" onClick={onSignInOpen} />
         </div>
       </Flex>
     </ComponentContainer>
