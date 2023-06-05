c8362acdca097e9a61dc523ed2c2908da8879aa6
Bong <qhdgkdbs@gmail.com>
2023-06-05 22:39:47 +0900

    🍻 shipping리스트 업데이트

--- packages/app/user-client/src/App/App.searchBar.tsx
+++ packages/app/user-client/src/App/App.searchBar.tsx
@@ -1,7 +1,17 @@
-import { Camera, ComponentContainer, Flex, Input } from '@common/ssung-ui/components'
+import {
+  Camera,
+  ComponentContainer,
+  Flex,
+  Input,
+} from '@common/ssung-ui/components'
 import styled from '@emotion/styled'
 import { Dispatch, memo, SetStateAction, useState } from 'react'
 import { useModal } from '@common/ssung-ui'
+import {
+  AiOutlineCamera,
+  AiOutlineCloseCircle,
+  AiOutlineSearch,
+} from 'react-icons/ai'
 
 interface SearchBarProps {
   searchID?: string
@@ -22,15 +32,21 @@ const SearchBar = (props: SearchBarProps) => {
     <ComponentContainer>
       <Flex alignItems={'center'} justifyContent={'center'} gap={'1rem'}>
         <Input
+          style={{ width: '100%' }}
           value={qrCodeResult}
           onChange={(e) => setQrCodeResult(e.target.value)}
+          placeholder="기분이 슝슝한 숭실대"
         />
         {cam && <Camera open={setCam} setResult={setQrCodeResult} />}
         <ActionButton onClick={() => setCam(!cam)}>
-          {cam ? 'X' : '카메라'}
+          {cam ? (
+            <AiOutlineCloseCircle size={24} color={'var(--text-gray)'} />
+          ) : (
+            <AiOutlineCamera size={24} color={'var(--text-gray)'} />
+          )}
         </ActionButton>
         <ActionButton style={{ zIndex: 0 }} onClick={fetchShipmentInfo}>
-          검색
+          <AiOutlineSearch size={24} color={'var(--text-gray)'} />
         </ActionButton>
       </Flex>
     </ComponentContainer>
@@ -41,4 +57,10 @@ export default memo(SearchBar)
 
 const ActionButton = styled.button`
   z-index: 2;
+  background: none;
+  color: inherit;
+  border: 0px;
+  border-radius: 0.25rem;
+  padding: 0.5rem 0.5rem;
+  outline: inherit;
 `
