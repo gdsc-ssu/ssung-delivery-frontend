48beadc6ba8ad3caec02a050d6a629a176a12258
Bong <qhdgkdbs@gmail.com>
2023-06-05 22:40:04 +0900

    🍻 카드스택제거

--- packages/app/user-client/src/App/App.shipping.tsx
+++ packages/app/user-client/src/App/App.shipping.tsx
@@ -1,28 +1,19 @@
 import {
   Card,
-  CSComponent,
   ComponentContainer,
   Flex,
   ShipHistory,
   Spacer,
   SubTitle,
 } from '@common/ssung-ui/components'
-import { memo } from 'react'
 import { SHIP_MOCK_DATA } from '../Model/ship.mock'
 
 const Shipping = () => {
   return (
     <ComponentContainer>
-      <SubTitle>배송 중</SubTitle>
       <Spacer height="1rem" />
       <Flex justifyContent={'space-between'}>
-        <CSComponent
-          elements={[
-            <Card shipInfo={SHIP_MOCK_DATA[0]} expandalble idx={0} />,
-            <Card shipInfo={SHIP_MOCK_DATA[1]} expandalble idx={1} />,
-            <Card shipInfo={SHIP_MOCK_DATA[2]} expandalble idx={2} />,
-          ]}
-        />
+        <Card shipInfo={SHIP_MOCK_DATA[0]} expandalble idx={0} />
         <ShipHistory shipInfo={SHIP_MOCK_DATA} />
       </Flex>
 
@@ -31,4 +22,4 @@ const Shipping = () => {
   )
 }
 
-export default memo(Shipping)
+export default Shipping
