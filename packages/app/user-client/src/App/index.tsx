ee17ca4b89a44248031e7efbdf1c46fad7ecaec2
Bong <qhdgkdbs@gmail.com>
2023-06-05 22:40:21 +0900

    🍻 임시 - 엠티 컴포넌트 활용

--- packages/app/user-client/src/App/index.tsx
+++ packages/app/user-client/src/App/index.tsx
@@ -3,15 +3,21 @@ import Recent from './App.recent'
 import SearchBar from './App.searchBar'
 import Shipping from './App.shipping'
 import { useCallback, useState } from 'react'
-import { BodyContainer, Spacer } from '@common/ssung-ui/components/Layout'
-import { CSProvider } from '@common/ssung-ui'
+import {
+  BodyContainer,
+  ComponentContainer,
+  Spacer,
+} from '@common/ssung-ui/components/Layout'
+import { SubTitle } from '@common/ssung-ui'
+import Empty from '../component/Empty'
 
 function App() {
   const [searchID, setSearchID] = useState<string>()
+  const [shipInfo, setShipInfo] = useState<any>(false)
   const onSearchShipping = useCallback(() => {}, [])
 
   return (
-    <CSProvider>
+    <>
       <BodyContainer>
         <Spacer height="3rem" />
 
@@ -25,14 +31,17 @@ function App() {
         />
         <Spacer height="3rem" />
 
-        <Shipping />
+        <ComponentContainer>
+          <SubTitle>배송 조회</SubTitle>
+          {shipInfo ? <Shipping /> : <Empty />}
+        </ComponentContainer>
         <Spacer height="3rem" />
 
         <Recent />
 
         <Spacer height="3rem" />
       </BodyContainer>
-    </CSProvider>
+    </>
   )
 }
 
