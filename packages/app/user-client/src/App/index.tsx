17f00165d42a850b39783da6be5e673a4651efd1
Bong <qhdgkdbs@gmail.com>
2023-06-07 17:10:03 +0900

    🍻 잘 작동하는 것 같은 친절한 검색 안내

--- packages/app/user-client/src/App/index.tsx
+++ packages/app/user-client/src/App/index.tsx
@@ -2,21 +2,24 @@ import Header from './App.header'
 import Recent from './App.recent'
 import SearchBar from './App.searchBar'
 import Shipping from './App.shipping'
-import { useCallback, useState } from 'react'
+import { useCallback, useRef, useState } from 'react'
 import {
   BodyContainer,
   ComponentContainer,
+  Flex,
   Spacer,
 } from '@common/ssung-ui/components/Layout'
 import { SubTitle } from '@common/ssung-ui'
 import Empty from '../component/Empty'
 import { SHIP_MOCK_DATA } from '../Model/ship.mock'
 import { ShipData } from '../Model/ship'
+import { Text } from '@/../../common/ssung-ui/components'
 
 function App() {
   const [searchID, setSearchID] = useState<string>('')
   const [shipInfo, setShipInfo] = useState<ShipData>()
   const [noData, setNodata] = useState<boolean>(false)
+  const [isInputFoucs, setIsInputFoucs] = useState<boolean>(false)
 
   const findShippingDataIdx = (key: string) => {
     return SHIP_MOCK_DATA.findIndex((obj) => obj.keywords === key)
@@ -31,6 +34,12 @@ function App() {
     setShipInfo(SHIP_MOCK_DATA[targetIdx])
   }, [searchID])
 
+  const initSearch = () => {
+    setSearchID('')
+    setShipInfo(undefined)
+    setIsInputFoucs(true)
+  }
+
   return (
     <>
       <BodyContainer>
@@ -40,6 +49,8 @@ function App() {
         <Spacer height="3rem" />
 
         <SearchBar
+          setIsInputFoucs={setIsInputFoucs}
+          isInputFoucs={isInputFoucs}
           searchID={searchID}
           setSearchID={setSearchID}
           onSearch={onSearchShipping}
@@ -47,17 +58,33 @@ function App() {
         <Spacer height="3rem" />
 
         <ComponentContainer>
-          <SubTitle>배송 조회</SubTitle>
+          <Flex justifyContent={'space-between'}>
+            <SubTitle>배송 조회</SubTitle>
+
+            <Text
+              color="var(--form-text)"
+              fontSize="0.875rem"
+              style={{ textDecoration: 'underline' }}
+              onClick={() => {
+                initSearch()
+              }}
+            >
+              {shipInfo ? '초기화' : '검색하기'}
+            </Text>
+          </Flex>
+
           {shipInfo ? (
             <Shipping />
           ) : (
-            <Empty
-              text={
-                noData
-                  ? '검색 가능한 배송 정보가 없습니다.'
-                  : '지금 검색을 해봐요!!!'
-              }
-            />
+            <div onClick={() => setIsInputFoucs(true)}>
+              <Empty
+                text={
+                  noData
+                    ? '검색 가능한 배송 정보가 없습니다.'
+                    : '지금 검색을 해봐요!!!'
+                }
+              />
+            </div>
           )}
         </ComponentContainer>
         <Spacer height="3rem" />
